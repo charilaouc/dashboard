06559f3f917e69ee624bc8ce8d23885aea41a0eb
CC <charilaou.c@gmail.com>
2023-09-30 18:03:19 +0300

    added heade and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import NavBar from './components/header/NavBar'
+import Footer from './components/footer/Footer'
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400','700'] })
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <div className="flex flex-col min-h-screen">
+          <NavBar/>
+          <main className="flex-grow">{children}</main>
+          <Footer/>
+        </div>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main>CosmosOrder Ltd</main>
+    <div>CosmosOrder Ltd</div>
   )
 }
