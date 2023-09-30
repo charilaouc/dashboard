114cc616da89dc716fd25a1eceeb862faa167782
CC <charilaou.c@gmail.com>
2023-10-01 00:10:42 +0300

    creating simple input

--- app/components/forms/input.tsx
+++ app/components/forms/input.tsx
@@ -1,6 +1,20 @@
 'use client'
 
-const Input = () => {
+import { UseFormRegister,FieldValues, FieldErrors} from "react-hook-form";
+
+interface InputProps{
+    id: string;
+    label:  string;
+    type?:  string;
+    disabled?:  boolean;
+    required?:  boolean;
+    register: UseFormRegister<FieldValues>
+    errors: FieldErrors
+}
+
+const Input: React.FC<InputProps> = ({
+
+}) => {
     return ( <div>
 
     </div> );
